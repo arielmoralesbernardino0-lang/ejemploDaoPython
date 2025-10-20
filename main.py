554472521db85b202c54f151dc007cfdfe4e47e4
Ariel <arielmoralesbernardino0@gmail.com>
2025-10-20 11:24:59 -0600

    new file:   20_10_2025 modified:   config.json new file:   dao/__pycache__/__init__.cpython-312.pyc new file:   dao/__pycache__/factory.cpython-312.pyc new file:   dao/__pycache__/user_dao_base.cpython-312.pyc new file:   dao/__pycache__/user_dao_mysql.cpython-312.pyc new file:   dao/__pycache__/user_dao_txt.cpython-312.pyc new file:   dao/__pycache__/user_dao_xml.cpython-312.pyc modified:   main.py new file:   models/__pycache__/user.cpython-312.pyc

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dao.factory import get_dao_from_config
 def main():
     dao = get_dao_from_config("config.json")
 
-    dao.add_user(User(None, "Luis Arturo", "luis@example.com"))
-    dao.add_user(User(None, "Armando", "armando@example.com"))
+    dao.add_user(User(None, "JSc ", "jusss@example.com"))
+    dao.add_user(User(None, "AMaria", "asere@gmail.com"))
 
     print("Usuarios almacenados:")
     for u in dao.get_all_users():
